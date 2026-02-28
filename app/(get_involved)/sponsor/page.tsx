2c078827c946822f594f4fc153d5514fd98aa3df
Gagan Preet Singh <gagan.preet.singh@hp.com>
2026-02-27 18:27:05 -0600

    sponsorship 2026

--- app/(get_involved)/sponsor/page.tsx
+++ app/(get_involved)/sponsor/page.tsx
@@ -22,7 +22,7 @@ export default async function SponsorPage({}: Props) {
   const sponsor_types: {key: number, value: string}[] = sponsor_types_raw.map((entityType) => {return {key: entityType.id, value: entityType.entityTypeName || ''}})
   return <>
     <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
-      Sponsorship for Vaisakhi Mela 2025
+      Sponsorship for Grand Vaisakhi Night 2026
     </h2>
     <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
       Sponsorship Packages
@@ -30,26 +30,24 @@ export default async function SponsorPage({}: Props) {
     <div className="my-6 w-full overflow-y-auto">
       <table className="w-full table-fixed min-w-[1150px]">
         <thead>
-          <TitleRowComponent item_title="Sponsorship Incentives" sponsor_packages={['Grand Sponsor', 'Platinum Sponsor', 'Diamond Sponsor', 'Gold Sponsor', 'Silver Sponsor', 'Bronze Sponsor', 'Rose Sponsor']} />
+          <TitleRowComponent item_title="Sponsorship Incentives" sponsor_packages={['Grand Sponsor', 'Diamond Sponsor', 'Platinum Sponsor', 'Gold Sponsor', 'Silver Sponsor', 'Bronze Sponsor', 'Rose Sponsor']} />
         </thead>
         <tbody>
           <RowComponent value="" checks={['$ 5,000', '$ 2,500', '$ 2,000', '$ 1,500', '$ 1,000', '$ 500', '$ 250']} />
-          <RowComponent value="Introducing YOU as a Brand at all media platforms" checks={['yes', 'yes', 'yes', 'yes', 'yes', 'no', 'no']} />
+          <RowComponent value="Introducing YOU as a Brand on all media platforms" checks={['yes', 'yes', 'yes', 'yes', 'yes', 'no', 'no']} />
           <RowComponent value="Award & Sponsor Recognition by Chief Official Host" checks={['yes', 'yes', 'yes', 'no', 'no', 'no', 'no']} />
-          <RowComponent value="Award & Sponsor Recognition" checks={['yes', 'yes', 'yes', 'yes', 'yes', 'no', 'no']} />
-          <RowComponent value="Tickets included" checks={['yes:20 VIP', 'yes:10 VIP', 'yes:8 VIP', 'yes:6 VIP', 'yes:4 VIP', 'yes:2 VIP', 'yes:1 Premium']} />
-          <RowComponent value="VIP Box seating option available" checks={['yes', 'yes', 'no', 'no', 'no', 'no', 'no']} />
+          <RowComponent value="Tickets included" checks={['yes:20 Premium', 'yes:10 Premium', 'yes:8 Premium', 'yes:6 Premium', 'yes:4 Premium', 'yes:2 Premium', 'yes:1 Complementary']} />
           <RowComponent value="Highlight spot to display business promotional material" checks={['yes', 'yes', 'yes', 'yes', 'yes', 'no', 'no']} />
-          <RowComponent value="Logo & Brand presence on website and all social media platform" checks={['yes', 'yes', 'yes', 'yes', 'yes', 'no', 'no']} />
-          {/* <RowComponent value="Complimentary food galore" checks={['yes:35 food coupons', 'yes:18 food coupons', 'yes:12 food coupons', 'yes:8 food coupons', 'yes:4 food coupons', 'yes:2 food coupons']} /> */}
-          <RowComponent value="Opportunity to present your brand during the program" checks={['yes:5 min', 'yes:3 min', 'no', 'no', 'no', 'no', 'no']} />
-          <RowComponent value="Recognition as host committee sponsor" checks={['yes', 'yes', 'no', 'no', 'no', 'no', 'no']} />
+          <RowComponent value="Logo & Brand presence on website and all social media platforms" checks={['yes', 'yes', 'yes', 'yes', 'yes', 'no', 'no']} />
+          <RowComponent value="Opportunity to present your brand during the program" checks={['no', 'no', 'yes', 'no', 'no', 'no', 'no']} />
+          <RowComponent value="Recognition as host committee sponsor" checks={['yes', 'no', 'yes', 'no', 'no', 'no', 'no']} />
           <RowComponent value="Recognition in digital print media" checks={['yes', 'yes', 'yes', 'yes', 'yes', 'no', 'no']} />
           <RowComponent value="Recognition in e-blast" checks={['yes', 'yes', 'yes', 'yes', 'yes', 'no', 'no']} />
           <RowComponent value="Recognition on website" checks={['yes', 'yes', 'yes', 'yes', 'yes', 'no', 'no']} />
           <RowComponent value="Recognition in event program" checks={['yes', 'yes', 'yes', 'yes', 'yes', 'no', 'no']} />
           <RowComponent value="Verbal recognition" checks={['yes', 'yes', 'yes', 'yes', 'yes', 'yes', 'no']} />
-          <RowComponent value="Color advertisement in the event souvenir" checks={['yes:Full page', 'yes:Full page', 'yes:1/2 page', 'yes:1/2 page', 'yes:1/4 page', 'no', 'no']} />
+          <RowComponent value="Color advertisement in the event souvenir" checks={['yes:Full page', 'yes:Half page', 'yes:Full page', 'yes:Half page', 'yes:Quarter page', 'no', 'no']} />
+          <RowComponent value="Flyers / Business cards on PSGH table & slide on screen" checks={['no', 'no', 'no', 'no', 'no', 'no', 'yes']} />
         </tbody>
       </table>
       {/* <iframe src="https://drive.google.com/file/d/1dgzvSGNsSYo-a9Cu1g7TZYJovBwztwZP/preview" width="100%" height="100%" allow="autoplay" style={{minHeight: '500px'}}></iframe> */}
@@ -66,7 +64,7 @@ export default async function SponsorPage({}: Props) {
           </a>
         </Button>
       </div>
-      <SponsorshipFormComponent event_code={'va25'} sponsor_types={sponsor_types} sponsorshipLevels={sponsorshipLevels} />
+      <SponsorshipFormComponent event_code={'va26'} sponsor_types={sponsor_types} sponsorshipLevels={sponsorshipLevels} />
     </div>
   </>
 }
